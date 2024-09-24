bedb509b2721ed15aeadac38267b3801123b9295
Jim Lakis <ejames.b1@gmail.com>
2024-09-24 14:43:26 -0700

    needed to add the name change when instaniating the class from ThreadingTaskManager() to ThreadPoolTaskManager()

--- main_threadPool.py
+++ main_threadPool.py
@@ -17,7 +17,7 @@ def main():
     max_number_of_tasks = 30
     number_of_threads_utilized = 8
 
-    task_manager = ThreadingTaskManager(max_number_of_tasks, number_of_threads_utilized)
+    task_manager = ThreadPoolTaskManager(max_number_of_tasks, number_of_threads_utilized)
     results_list = task_manager.run()
 
     ## Creating log_entry
