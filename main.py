e00271a06750a9c717d065a03797668ba2417c14
Jim Lakis <ejames.b1@gmail.com>
2024-09-25 13:29:13 -0700

    just renaming previous main.py to main_initial_example.py. Also renaming previous whiteboard.py to main.py. This new main.py is now the primary entry-point for this project.

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+from threading_manager import ThreadingTaskManager ## custom module
+from threadPool_manager import ThreadPoolTaskManager ## custom module
 
 from os import getpid
-
-from queue import Queue, Empty
-from threading import Thread, Lock
-
-from random import randrange
-from time import sleep
 
 from pathlib import Path
 import json
@@ -16,71 +12,49 @@ def append_JSON_file(path, log_entry):
         json.dump(log_entry, textiowrapper)
         textiowrapper.write('\n')
 
-class ThreadSafeList:
-    def __init__(self):
-        self.data = []
-        self.lock = Lock()
-
-    def append(self, item):
-        with self.lock:
-            self.data.append(item)
-
-    def get_data(self):
-        with self.lock:
-            return self.data.copy()
-
-def worker(thread_id, task_queue, result_list):
-    while True:
-        try:
-            ## block=False - If the queue is empty, no more tasks to process, do not have a thread block/"wait". This throws the Empty exception.
-            item = task_queue.get(block=False)
-            ## Simulate work taking variable time to complete
-            sleep(randrange(0, 2))
-            temp_tuple = (int(thread_id), int(item))
-            result_list.append(temp_tuple)
-            task_queue.task_done()
-        ## Exit the Queue when threads no longer have tasks assigned
-        except Empty:
-            break
 
 def main():
+
     max_number_of_tasks = 30
     number_of_threads_utilized = 8
-    task_queue = Queue()
-    result_list = ThreadSafeList()
 
-    ## Creating the list-like Queue of tasks: [0,1,2,...29]
-    for task_id in range(max_number_of_tasks):
-        task_queue.put(task_id)
+    while True:
+        try:
+            user_input = int(input("\nEnter 1 to run the task utilizing the 'threading' module,\nenter 2 to run the task utilizing the 'concurrent.futures' module or\nenter 0 to exit the program: "))
 
-    ## Start the worker threads
-    threads = []
-    for thread_id in range(number_of_threads_utilized):
-        ## Define a thread. Daemon thread is by default False thus not allowing the main thread to exit until these sub-threads have finished running.
-        thread = Thread(target=worker, args=(thread_id, task_queue, result_list))
-        ## Add the thread to a list in order to manage it
-        threads.append(thread)
-        ## Start the thread
-        thread.start()
+            if user_input == 0:
+                print("Exiting the program without performing the task...\n")
+                break
+
+            elif user_input == 1:
+                task_manager = ThreadingTaskManager(max_number_of_tasks, number_of_threads_utilized)
+                results_list = task_manager.run_tasks()
+
+            elif user_input == 2:
+                task_manager = ThreadPoolTaskManager(max_number_of_tasks, number_of_threads_utilized)
+                results_list = task_manager.run()
+
+            else:
+                print("Invalid input. You must enter either 0, 1 or 2, exiting the program...\n")
+                break
+
+        except ValueError:
+            print("Invalid input type, exiting the program...\n")
+            break
         
-    ## Wait for all threads to complete
-    for thread in threads:
-        thread.join()
-
-    ## Creating log_entry
-    log_entry = {"metadata": {
-        "utilizedNumberOfThreads": number_of_threads_utilized,
-        "maxNumberOfTasks": max_number_of_tasks
-        },
-        "orderedThreadIdTaskIdPairs": result_list.get_data()
-        }
-
-    ## Append JSON file with log_entry
-    path = Path(r".\thread_logs.JSON")
-    append_JSON_file(path, log_entry)
+        ## Creating log_entry in JSON format
+        log_entry = {"metadata": {
+            "utilizedNumberOfThreads": number_of_threads_utilized,
+            "maxNumberOfTasks": max_number_of_tasks
+            },
+            "orderedThreadIdTaskIdPairs": results_list
+            }
+        
+        ## Append JSON file with log_entry
+        path = Path(r".\thread_logs.JSON")
+        append_JSON_file(path, log_entry)
 
 
 if __name__ == "__main__":
-    print(f"process_id: {getpid()}\n")
+    print(f"\nprocess_id: {getpid()}")
     main()
-    
