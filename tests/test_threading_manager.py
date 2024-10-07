f1498880713589b964b21f4a28065ff211088437
Jim Lakis <ejames.b1@gmail.com>
2024-10-07 15:02:10 -0700

    amending testing for threading_manager.py in test_threading_manager.py to add testing of functionality of the Lock object

--- tests/test_threading_manager.py
+++ tests/test_threading_manager.py
@@ -3,6 +3,7 @@ import pytest
 from src.threading_manager import ThreadSafeList
 from threading import Lock
 
+
 def test_passes():
     assert True
 
@@ -11,10 +12,22 @@ class TestThreadSafeList:
     def test_init(self):
         obj = ThreadSafeList()
 
-        ## Test initial state
+        ## Test object's initial state
         assert isinstance(obj.data, list)
         assert len(obj.data) == 0
 
-        # Test lock initialization
-        assert hasattr(obj.lock, 'acquire')
-        assert hasattr(obj.lock, 'release')
+    def test_lock_functionality(self):
+        obj = ThreadSafeList()
+        
+        ## Test that the lock can be acquired
+        assert obj.lock.acquire(blocking=False)  # Should be able to acquire the lock
+        
+        ## Test that the lock can't be acquired again while held
+        assert not obj.lock.acquire(blocking=False)  # Should not be able to acquire the lock again
+        
+        ## Release the lock
+        obj.lock.release()
+        
+        ## Test that the lock can be acquired again after release
+        assert obj.lock.acquire(blocking=False)  ## Should be able to acquire the lock again
+        obj.lock.release()  ## Clean up by releasing the lock
